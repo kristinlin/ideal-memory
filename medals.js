18d50a7296522175463af419bf113287a98a5d58
MrEricL <eli6@stuy.edu>
2018-03-14 00:45:56 -0400

    fin

--- medals.js
+++ medals.js
@@ -1,9 +1,22 @@
+var svg = document.getElementById("place");
+var canada = document.getElementById("CA");
+var norway = document.getElementById("NOR");
 
 
-d3.selectAll(“circle”).style(“fill”,
-function(){
-	console.log("hi");
-	return "gold";
+var ca = [11,8,10]
+var nor = [14,14,11]
 
-})
+var hail_canada = function (e) {
+    var count = d3.selectAll("circle").data(ca);
+    count.attr("r", function(d){return d*8;});
+}
 
+canada.addEventListener('click', hail_canada);
+
+var hail_norway = function (e) {
+	var count = d3.selectAll("circle").data(nor);
+	count.attr("r", function(d){return d*8;});
+
+}
+
+norway.addEventListener('click', hail_norway);
